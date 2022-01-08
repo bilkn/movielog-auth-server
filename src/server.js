816bb5962630724b7dc8f8333ca5f5939faa26ab
bilkn <bilkan.konus@gmail.com>
2022-01-08 11:35:42 +0300

    feat: add rate limiter

--- src/server.js
+++ src/server.js
@@ -2,14 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const { AuthRouter } = require("./routes");
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 15,
+});
 
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-
+app.use(limiter);
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 app.get("/", (req, res) => {
